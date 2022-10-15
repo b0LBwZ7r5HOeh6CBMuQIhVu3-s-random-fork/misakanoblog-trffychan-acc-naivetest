523ff336811e0d1eb494b24e9053c00a4479ad81
永雏塔菲 <108621198+taffychan@users.noreply.github.com>
2022-10-16 02:49:31 +0800

    Update naiveproxy.sh

--- naiveproxy.sh
+++ naiveproxy.sh
@@ -62,12 +62,20 @@ instnaive(){
     ~/go/bin/xcaddy build --with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive
     mkdir /opt/naive
     mv ./caddy /opt/naive/caddy
+    rm -f /root/go
+    
+    read -rp "请输入需要使用在NaiveProxy的域名：" domain
+    read -rp "请输入NaiveProxy的用户名 [默认随机生成]：" proxyname
+    [[ -z $proxyname ]] && proxyname=$(date +%s%N | md5sum | cut -c 1-8)
+    read -rp "请输入NaiveProxy的密码 [默认随机生成]：" proxypwd
+    [[ -z $proxypwd ]] && proxypwd=$(cat /proc/sys/kernel/random/uuid)
+    
     cat << EOF >/opt/naive/Caddyfile
-:443, naive.buliang0.tk
+:443, $domain
 tls admin@seewo.com
 route {
  forward_proxy {
-   basic_auth user pass
+   basic_auth $proxyname $proxypwd
    hide_ip
    hide_via
    probe_resistance
@@ -78,5 +86,75 @@ route {
   }
 }
 EOF
+    cat > /root/naive-client.json <<EOF
+{
+  "listen": "socks://127.0.0.1:1080",
+  "proxy": "https://${proxyname}:${proxypwd}@${domain}",
+  "log": ""
+}
+    qvurl="naive+https://${proxyname}:${proxypwd}@${domain}:443?padding=false#Naive"
+    echo $qvurl > /root/naive-qvurl.txt
+    
+    cd /opt/naive
     /opt/naive/caddy start
 }
+
+uninstallProxy(){
+    cd /opt/naive
+    /opt/naive/caddy stop
+    rm -rf /opt/naive
+    rm -f /root/naive-qvurl.txt /root/naive-client.json
+}
+
+startProxy(){
+    cd /opt/naive
+    /opt/naive/caddy start
+    green "NaiveProxy 已启动成功！"
+}
+
+stopProxy(){
+    cd /opt/naive
+    /opt/naive/caddy stop
+    green "NaiveProxy 已停止成功！"
+}
+
+reloadProxy(){
+    cd /opt/naive
+    /opt/naive/caddy reload
+    green "NaiveProxy 已重启成功！"
+}
+
+menu(){
+    clear
+    echo "#############################################################"
+    echo -e "#                  ${RED}NaiveProxy  一键配置脚本${PLAIN}                 #"
+    echo -e "# ${GREEN}作者${PLAIN}: MisakaNo の 小破站                                  #"
+    echo -e "# ${GREEN}博客${PLAIN}: https://blog.misaka.rest                            #"
+    echo -e "# ${GREEN}GitHub 项目${PLAIN}: https://gitlab.com/blog-misaka               #"
+    echo -e "# ${GREEN}GitLab 项目${PLAIN}: https://gitlab.com/misakablog                #"
+    echo -e "# ${GREEN}Telegram 频道${PLAIN}: https://t.me/misakablogchannel             #"
+    echo -e "# ${GREEN}Telegram 群组${PLAIN}: https://t.me/+CLhpemKhaC8wZGIx             #"
+    echo -e "# ${GREEN}YouTube 频道${PLAIN}: https://suo.yt/8EOkDib                      #"
+    echo "#############################################################"
+    echo ""
+    echo -e "  ${GREEN}1.${PLAIN}  安装 NaiveProxy"
+    echo -e "  ${GREEN}2.${PLAIN}  ${RED}卸载 NaiveProxy${PLAIN}"
+    echo " -------------"
+    echo -e "  ${GREEN}3.${PLAIN}  启动 NaiveProxy"
+    echo -e "  ${GREEN}4.${PLAIN}  停止 NaiveProxy"
+    echo -e "  ${GREEN}5.${PLAIN}  重载 NaiveProxy"
+    echo " -------------"
+    echo -e "  ${GREEN}0.${PLAIN} 退出"
+    echo ""
+    read -rp " 请输入选项 [0-5] ：" answer
+    case $answer in
+        1) installProxy ;;
+        2) uninstallProxy ;;
+        3) startProxy ;;
+        4) stopProxy ;;
+        5) reloadProxy ;;
+        *) red "请输入正确的选项 [0-5]！" && exit 1 ;;
+    esac
+}
+
+menu
